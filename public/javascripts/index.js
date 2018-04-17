b2c27bca35a90b984eb7318e6dafffde6065867f
Luke Slater <lxs511@bham.ac.uk>
2018-04-17 12:28:25 +0100

    bland and altman

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -22,6 +22,86 @@ var addListeners = function() {
   });
 }
 
+var showResults = function(c) {
+  drawBlandAndAltman(c);
+}
+
+var drawBlandAndAltman = function(c) {
+  var x = [], // Mean 
+      y = []; // Difference
+  
+  $('#'+c).find('tbody').find('tr').each(function() { // btw jquery's map sux
+    var val1 = parseInt($(this.cells[1]).find('input:first').val()),
+        val2 = parseInt($(this.cells[2]).find('input:first').val());
+
+    x.push(parseInt($(this.cells[3]).find('.avg')[0].innerHTML));
+    y.push(parseInt(val1) - parseInt(val2));
+  });
+
+  var upperAgreement = Math.max(...y);
+  var lowerAgreement = Math.min(...y);
+  var lineLength = Math.max(...x);
+  var bias = y.reduce((a, b) => a + b, 0) / y.length;
+
+  var trace = {
+    'x': x,
+    'y': y,
+    'mode': 'markers',
+    'type': 'scatter',
+    'name': 'Measurements'
+  };
+
+  var layout = {
+    'title': 'Bland and Altman Plot',
+    'showLegend': true,
+    'xaxis': {
+      'title': 'Mean'
+    },
+    'yaxis': {
+      'title': 'Difference'
+    },
+    'shapes': [
+      {
+        'type': 'line',
+        'xref': 'paper',
+        'x0': 0,
+        'y0': upperAgreement,
+        'x1': lineLength,
+        'y1': upperAgreement,
+        'line': {
+          'color': 'rgb(0, 0, 155)',
+          'width': 2
+        }
+      },
+      {
+        'type': 'line',
+        'xref': 'paper',
+        'x0': 0,
+        'y0': lowerAgreement,
+        'x1': lineLength,
+        'y1': lowerAgreement,
+        'line': {
+          'color': 'rgb(155, 0, 0)',
+          'width': 2
+        }
+      },
+      {
+        'type': 'line',
+        'xref': 'paper',
+        'x0': 0,
+        'y0': bias,
+        'x1': lineLength,
+        'y1': bias,
+        'line': {
+          'color': 'rgb(0, 155, 0)',
+          'width': 2
+        }
+      }
+    ]
+  };
+
+  Plotly.newPlot('bland', [trace], layout)
+}
 
 $(document).ready(function() {
   addListeners();

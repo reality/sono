818b66cdc7c4ec35e88274389b7047c077e7f108
Luke Slater <lxs511@bham.ac.uk>
2018-04-17 10:58:39 +0100

    calculate averages

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -3,9 +3,26 @@ var addNewRow = function(tb) {
       last = tbl.find('tr:last'),
       trNew = last.clone();
 
-console.log(trNew);
   var idx = trNew[0].cells[0]
   idx.innerHTML = parseInt(idx.innerHTML) + 1
 
   last.after(trNew);
+
+  addListeners();
 }
+
+var addListeners = function() {
+  $('.valueInput').keyup(function(e) {
+    var tr = e.currentTarget.parentElement.parentElement,
+        val1 = parseInt($(tr.cells[1]).find('input:first').val()),
+        val2 = parseInt($(tr.cells[2]).find('input:first').val()),
+        avg = $(tr.cells[3]).find('.avg')[0];
+
+    avg.innerHTML = (val1*1 + val2*1) / 2;
+  });
+}
+
+
+$(document).ready(function() {
+  addListeners();
+});
